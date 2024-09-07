ed6936a58c5438ebe74a27e3d2f30b94c1bc371e
Jisu <hellojs242@gmail.com>
2024-09-07 21:32:38 +0900

    :hammer: chore: swagger 설정 #2

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { ApiModule } from './api.module';
 
+const config = new DocumentBuilder()
+  .setTitle('Aletheia API')
+  .setDescription('금 거래소 서비스: Aletheia API 문서')
+  .setVersion('1.0')
+  .addTag('Aletheia')
+  .build();
+
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
